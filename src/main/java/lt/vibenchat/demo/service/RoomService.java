b6cfcb9b25b1e5abde4b53e87d450c2093ce8ff9
Zydrunas <Znorvilas28@gmail.com>
2024-01-19 21:34:53 +0200

    [Back-End][fix] fixed room search

--- src/main/java/lt/vibenchat/demo/service/RoomService.java
+++ src/main/java/lt/vibenchat/demo/service/RoomService.java
@@ -1,15 +1,19 @@
 package lt.vibenchat.demo.service;
 
+import lombok.extern.log4j.Log4j2;
 import lt.vibenchat.demo.Mapper;
 import lt.vibenchat.demo.dao.RoomDao;
 import lt.vibenchat.demo.dto.RoomDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Service
+@Log4j2
 public class RoomService {
     private final RoomDao roomDao;
     private final Mapper mapper;
@@ -41,17 +45,24 @@ public class RoomService {
     }
 
     public List<RoomDto> searchRoomsByNameAndGenre(String name, String genre) {
-        List<RoomDto> rooms = roomDao.getAll().stream().map(mapper::toRoomDto).toList();
+        List<RoomDto> rooms = roomDao.getAll().stream()
+                .map(mapper::toRoomDto)
+                .collect(Collectors.toList());
 
-        if (name == null) {
+        if (name.isEmpty() && !genre.equalsIgnoreCase("All")) {
+            System.out.println("veikia");
              return rooms.stream()
                      .filter(room -> room.getGenre().equals(genre))
                      .collect(Collectors.toList());
-        } else {
+        } else if (name.isEmpty() && genre.equalsIgnoreCase("All")) {
+            return rooms;
+        } else if (!name.isEmpty()){
             return rooms.stream()
-                    .filter(room -> room.getName().contains(name))
-                    .filter(room -> room.getGenre().equals(genre))
+                    .filter(room -> room.getName().toLowerCase().contains(name.toLowerCase()))
                     .collect(Collectors.toList());
+        } else {
+            log.error("Room search failed!!");
+            return new ArrayList<>();
         }
     }
 

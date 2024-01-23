da4e8ba8d1029f77e7782b326ccfbbc84ecc1c6b
Zydrunas <Znorvilas28@gmail.com>
2024-01-23 17:53:43 +0200

    [Front-End] added ability to enter a room

--- src/main/java/lt/vibenchat/demo/controller/RoomController.java
+++ src/main/java/lt/vibenchat/demo/controller/RoomController.java
@@ -6,6 +6,8 @@ import lt.vibenchat.demo.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,6 +26,12 @@ public class RoomController {
     public String SearchRoom(Model model, @RequestParam String genre, @RequestParam(name = "room-name") String name) {
         List<RoomDto> rooms =  service.searchRoomsByNameAndGenre(name,genre);
         model.addAttribute("roomList",rooms);
-        return "pages/index";
+        return "index";
+    }
+
+    @GetMapping("/room/{roomId}/")
+    public String Room(Model model, @PathVariable String roomId) {
+
+        return "room";
     }
 }

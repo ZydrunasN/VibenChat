9605ce9ab2eae5b3998cc5910380eab8e5d0992e
Zydrunas <Znorvilas28@gmail.com>
2024-02-02 16:35:22 +0200

    [Back-End] return full list of rooms on index call.

--- src/main/java/lt/vibenchat/demo/controller/MainController.java
+++ src/main/java/lt/vibenchat/demo/controller/MainController.java
@@ -1,13 +1,23 @@
 package lt.vibenchat.demo.controller;
 
+import lt.vibenchat.demo.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    private final RoomService service;
+
+    @Autowired
+    public MainController(RoomService service) {
+        this.service = service;
+    }
 
     @GetMapping
-    public String home () {
+    public String home (Model model) {
+        model.addAttribute("roomList",service.getListOfRooms());
         return "index";
     }
 

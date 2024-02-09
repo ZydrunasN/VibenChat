8dc995e5eae3bcf98de1f87ce20786ffcc02aa7f
Zydrunas <Znorvilas28@gmail.com>
2024-02-09 19:07:15 +0200

    [Back-End] added master user to properties, moved several DTO's to different directory

--- src/main/java/lt/vibenchat/demo/dto/inputValidateDto/user/LoginUserDto.java
+++ src/main/java/lt/vibenchat/demo/dto/inputValidateDto/user/LoginUserDto.java
@@ -1,4 +1,4 @@
-package lt.vibenchat.demo.dto.inputValidateDto;
+package lt.vibenchat.demo.dto.inputValidateDto.user;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
--- src/main/java/lt/vibenchat/demo/dto/inputValidateDto/user/RegisterUserDto.java
+++ src/main/java/lt/vibenchat/demo/dto/inputValidateDto/user/RegisterUserDto.java
@@ -1,4 +1,4 @@
-package lt.vibenchat.demo.dto.inputValidateDto;
+package lt.vibenchat.demo.dto.inputValidateDto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
